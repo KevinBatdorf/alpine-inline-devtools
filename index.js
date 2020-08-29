7618ba8294f75fa981a8393d3a796295c6cc1a3b
Kevin Batdorf <kbat82@gmail.com>
2020-08-29 09:41:28 +0700

    wip

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-window.devtools = function (position) {
+window.alpineDevTools = function(position) {
 	return {
-		show: false,
-		alpines: [],
-		observer: null,
-		position: position || 'right',
+        alpines: [],
+        open: false,
+        observer: null,
+        windowRef: null,
 		start() {
-			this.insertHTML()
 			this.alpines = null
 			this.alpines = document.querySelectorAll('[x-data]:not(#alpine-devtools)')
 			this.observer = new MutationObserver(mutations => {
 				this.updateAlpines()
 			})
 			this.registerEventsFromComponentsOnWindow()
-			this.alpines && this.wrapAlpines()
-		},
-		getAlpineData(alpine) {
-			if (!alpine.__x) return []
-			return Object.entries(alpine.__x.getUnobservedData())
+            this.alpines && this.wrapAlpines()
+
+            // If the window is already open, refresh it
+            if (sessionStorage.getItem('alpine-devtools')) {
+                this.openWindow()
+                this.updateAlpines()
+            }
 		},
 		registerEventsFromComponentsOnWindow() {
 			let events = []
@@ -40,14 +41,117 @@ window.devtools = function (position) {
 			})
 		},
 		updateAlpines() {
-			this.alpines = document.querySelectorAll('[x-data]:not(#alpine-devtools)')
+            this.alpines = document.querySelectorAll('[x-data]:not(#alpine-devtools)')
+            if (this.windowRef) {
+                this.windowRef.alpines = this.alpines
+                const viewer = this.windowRef.document.querySelector('#alpine-devtools-viewer')
+                if (!viewer) return
+                typeof viewer.__x !== 'undefined' && viewer.__x.updateElements(viewer)
+            }
 		},
-		computeTitle(alpine) {
+		openWindow() {
+            this.windowRef = window.open('', 'alpine-devtools', 'width=400, height=650, left=100, top=100')
+            if (!this.windowRef) return sessionStorage.removeItem('alpine-devtools')
+            this.windowRef.document.body.style.backgroundColor = '#1a202c'
+            this.windowRef.document.body.innerHTML = ''
+            this.windowRef.document.title = 'Alpine DevTools'
+            this.windowRef.alpines = this.alpines
+
+            const alpineScript = this.windowRef.document.createElement('script')
+            alpineScript.src = 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js'
+            this.windowRef.document.head.appendChild(alpineScript)
+
+            const tailwindCSS = this.windowRef.document.createElement('link')
+            tailwindCSS.href = 'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css'
+            tailwindCSS.rel = 'stylesheet'
+            this.windowRef.document.head.appendChild(tailwindCSS)
+
+            const devtoolsScript = this.windowRef.document.createElement('script')
+            devtoolsScript.setAttribute('type', 'text/javascript')
+            devtoolsScript.src = document.getElementById('alpine-devtools-script').src
+            this.windowRef.document.head.appendChild(devtoolsScript)
+
+            this.setUpPopupData()
+        },
+        setUpPopupData() {
+            const viewer = this.windowRef.document.createElement('div')
+            viewer.id = 'alpine-devtools-viewer'
+            viewer.setAttribute('x-data', 'window.alpineDevToolsViewer(window.alpines)')
+            viewer.innerHTML = this.viewerHTML()
+
+            // Add the reference to the session so we don't need to reopen the popup everytime
+            sessionStorage.setItem('alpine-devtools', this.windowRef.name)
+
+            this.open = true
+            setTimeout(() => {
+                this.windowRef.document.body.appendChild(viewer)
+            }, 500)
+
+            this.windowRef.addEventListener('beforeunload', () => {
+                sessionStorage.removeItem('alpine-devtools')
+                this.windowRef = null
+                this.open = false
+            })
+        },
+        viewerHTML() {
+            return `
+            <div
+                class="flex flex-col justify-between fixed inset-0 bg-gray-900 text-gray-400 pb-3 pt-6 max-w-screen overflow-hidden"
+                x-cloak
+                x-show="open"
+				x-transition:enter="transition ease-in duration-200"
+				x-transition:enter-start="transform opacity-100 translate-y-2"
+				x-transition:enter-end="transform opacity-100"
+				x-transition:leave="transition ease-out duration-300"
+				x-transition:leave-start="transform translate-y-0 opacity-100"
+				x-transition:leave-end="transform translate-y-2 opacity-0">
+				<div
+					class="divide-y-2 divide-gray-700 space-y-5 -mt-5 mb-5 px-3 overflow-scroll">
+					<template x-for="alpine of alpines">
+						<div class="pt-5">
+							<div x-text="computeTitle(alpine)" class="mb-1 font-extrabold" style="color:#d8dee9"></div>
+							<template x-if="!getAlpineData(alpine).length"><p class="text-sm">No data found</p></template>
+							<template x-for="[key, value] of getAlpineData(alpine)">
+								<div
+									class="leading-normal"
+									x-html="getItem(key, value)"
+									x-show="getType(value) !== 'function'">
+								</div>
+							</template>
+						</div>
+					</template>
+				</div>
+				<div class="border-t border-gray-700 pt-2 text-xs leading-none mx-3">
+					<a class="hover:text-blue-500 mr-1" target="_blank" href="https://twitter.com/kevinbatdorf">@kevinbatdorf</a>
+					·
+					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://github.com/kevinbatdorf/alpine-inline-devtools">github</a>
+					·
+					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://github.com/sponsors/KevinBatdorf">sponsor</a>
+					·
+					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://codepen.io/collection/nRxrPk">Alpine demos</a>
+				</div>
+			</div>`
+		}
+	}
+}
+
+window.alpineDevToolsViewer = function(alpines) {
+    Alpine.addMagicProperty('devtoolsRefresh', function ($el) {
+        return () => {
+            this.updateElements()
+        }
+    })
+    return {
+        computeTitle(alpine) {
 			return alpine.getAttribute('x-title')
 				|| alpine.getAttribute('aria-label')
 				|| alpine.id
 				|| this.convertFunctionName(alpine.getAttribute('x-data'))
 				|| `<${alpine.tagName.toLowerCase()}>`
+        },
+        getAlpineData(alpine) {
+			if (!alpine.__x) return []
+			return Object.entries(alpine.__x.getUnobservedData())
 		},
 		getType(value) {
 			if (Array.isArray(value)) {
@@ -102,53 +206,22 @@ window.devtools = function (position) {
 			div.innerText = html
 			return div.innerHTML
 		},
-		insertHTML() {
-			this.$el.innerHTML = `<button
-				x-show.immediate="!show"
-				style="background-color:rgb(138, 192, 207);border-color:#d8dee9;min-width:0;"
-				:class="position + '-0'"
-				class="border border-3 bottom-0 fixed focus:outline-none font-black h-10 mb-3 mx-4 rounded-full text-gray-900 hover:text-white shadow-2xl text w-10"
-				@click="show = true">A</button>
-			<div
-				style="width:500px;z-index:2147483647;"
-				:class="position + '-0'"
-				class="fixed bottom-0 my-3 mx-4 bg-gray-900 text-gray-400 pb-3 pt-6 max-w-screen shadow-2xl rounded-lg overflow-hidden"
-				x-cloak
-				x-show="show"
-				x-transition:enter="transition ease-in duration-200"
-				x-transition:enter-start="transform opacity-100 translate-y-2"
-				x-transition:enter-end="transform opacity-100"
-				x-transition:leave="transition ease-out duration-300"
-				x-transition:leave-start="transform translate-y-0 opacity-100"
-				x-transition:leave-end="transform translate-y-2 opacity-0">
-				<button style="min-width:0" class="absolute top-0 right-0 mr-2 bg-gray-900 font-mono focus:outline-none" @click="show = false">x</button>
-				<div
-					style="max-height:70vh"
-					class="divide-y-2 divide-gray-700 space-y-5 -mt-5 mb-5 px-3 overflow-scroll">
-					<template x-for="alpine of alpines">
-						<div class="pt-5">
-							<div x-text="computeTitle(alpine)" class="mb-1 font-extrabold" style="color:#d8dee9"></div>
-							<template x-if="!getAlpineData(alpine).length"><p class="text-sm">No data found</p></template>
-							<template x-for="[key, value] of getAlpineData(alpine)">
-								<div
-									class="leading-normal"
-									x-html="getItem(key, value)"
-									x-show="getType(value) !== 'function'">
-								</div>
-							</template>
-						</div>
-					</template>
-				</div>
-				<div class="border-t border-gray-700 pt-2 text-xs leading-none mx-3">
-					<a class="hover:text-blue-500 mr-1" target="_blank" href="https://twitter.com/kevinbatdorf">@kevinbatdorf</a>
-					·
-					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://github.com/kevinbatdorf">github</a>
-					·
-					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://github.com/sponsors/KevinBatdorf">sponsor</a>
-					·
-					<a class="hover:text-blue-500 mx-1" target="_blank" href="https://codepen.io/collection/nRxrPk">more demos</a>
-				</div>
-			</div>`
-		}
-	}
+    }
 }
+
+const alpine = window.deferLoadingAlpine || ((alpine) => alpine())
+window.deferLoadingAlpine = function (callback) {
+    alpine(callback)
+    const alpineDevToolsComponent = document.createElement('button')
+    alpineDevToolsComponent.id = 'alpine-devtools'
+    alpineDevToolsComponent.setAttribute('x-data', 'alpineDevTools()')
+    alpineDevToolsComponent.setAttribute('x-show.transition.opacity.duration.1000', 'alpines.length && !open')
+    alpineDevToolsComponent.setAttribute('x-on:click', 'openWindow')
+    alpineDevToolsComponent.setAttribute('x-data', 'alpineDevTools()')
+    alpineDevToolsComponent.setAttribute('x-init', '$nextTick(() => { start() })')
+    alpineDevToolsComponent.textContent = 'Alpine Devtools'
+    alpineDevToolsComponent.style.cssText = "position:fixed!important;bottom:0!important;right:0!important;margin:4px!important;padding:5px 8px!important;border-radius:10px!important;background-color:#1a202c!important;color:#d8dee9!important;font-size:14px!important"
+    document.body.appendChild(alpineDevToolsComponent)
+}
+
+export default alpineDevTools
